8cc0be6ef4df3dbfb0f5624c4f495e22ebb35067
nando <muhammad.rezki.ananda@gmail.com>
2021-08-03 20:27:06 +0700

    Set theme | Finish

--- app/src/main/java/com/mrrezki/tugas/MainActivity.kt
+++ app/src/main/java/com/mrrezki/tugas/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.mrrezki.tugas
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
