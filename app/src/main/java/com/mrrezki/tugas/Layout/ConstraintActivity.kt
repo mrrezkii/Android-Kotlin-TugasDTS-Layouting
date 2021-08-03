52e456c6fd9a67a99f72308fb745deeab10b4f68
nando <muhammad.rezki.ananda@gmail.com>
2021-08-03 21:16:17 +0700

    Refactoring

---
+++ app/src/main/java/com/mrrezki/tugas/Layout/ConstraintActivity.kt
@@ -0,0 +1,25 @@
+package com.mrrezki.tugas.Layout
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.mrrezki.tugas.R
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
---
+++ app/src/main/java/com/mrrezki/tugas/Layout/RelativeActivity.kt
@@ -0,0 +1,25 @@
+package com.mrrezki.tugas.Layout
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.mrrezki.tugas.R
+
+class RelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
